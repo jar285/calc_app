cca99e042e730e9225d6aebe8a796444d06727bb
Jesus <jar285@njit.edu>
2024-02-05 15:12:10 -0500

    Fixed Case sensitive class name, and fixed encapsulation

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,18 @@
 '''Testing the calculator class'''
-from calculator import calculator
+from calculator import Calculator
 
-def test_calculator():
+def test_add():
     '''Test that calculator addind works'''
-    assert calculator.add(2,2) == 4
+    assert Calculator.add(2,2) == 4
+
+def test_subtract():
+    '''Test that calculator substraction works'''
+    assert Calculator.subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that calculator multiply works'''
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that calculator divide works'''
+    assert Calculator.divide(2,2) == 1

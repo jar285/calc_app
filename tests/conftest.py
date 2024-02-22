32e4b30bd4c32d70de7b9654a35cf57c57681629
Jesus <jar285@njit.edu>
2024-02-21 23:57:43 -0500

    modified conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,15 @@
-'''imported pytest and faker to conftest.py'''
+# conftest.py
 import pytest
 from faker import Faker
-from calculator import Calculator
 
 @pytest.fixture(scope='session')
 def fake_data():
     """Provide a Faker instance for generating fake data."""
     return Faker()
 
+def pytest_addoption(parser):
+    parser.addoption("--num_records", action="store", default=10, type=int, help="Number of records to generate")
 
-def test_addition(fake_data):
-    """Addition function"""
-    a = fake_data.random_number(digits=2)
-    b = fake_data.random_number(digits=2)
-    expected_result = a + b
-    assert Calculator.add(a, b) == expected_result
-    assert Calculator.get_last_calculation() == f"Added {a} to {b} got {expected_result}"
+@pytest.fixture
+def num_records(request):
+    return request.config.getoption("--num_records")
